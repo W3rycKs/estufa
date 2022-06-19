cba40d74758b8ff0e53ecd078eed85a50d2f0972
W3rycKs <68777936+W3rycKs@users.noreply.github.com>
2022-06-19 14:15:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -368,8 +368,8 @@ function desenhar_temperatura(array) {
 	media+=obj.temp
 	valor = obj.temp
 	ctx.strokeStyle = "#cc3131";
-	var aleatorio = Math.random()*2;
-    ctx.lineTo(x, altura-(valor*aleatorio)); //definir desnivel
+	//var aleatorio = Math.random()*2;
+     	ctx.lineTo(x, altura-(valor*10*valor/6));
 	ctx.stroke();
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, largura, 30);
